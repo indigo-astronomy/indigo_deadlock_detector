453857462ff7ca87397a296a11e6ff1b839a800d
Rumen G.Bogdanovski <rumen@skyarchive.org>
2020-10-21 14:44:34 +0300

    add -p option; better pid retrieval

--- indigo_deadlock_detector.sh
+++ indigo_deadlock_detector.sh
@@ -10,17 +10,30 @@ PRINT_VARS=""
 while [[ "$#" -gt 0 ]]; do
 	case $1 in
 		-v|--print-variables) PRINT_VARS="-full"; shift ;;
+		-p|--pid) PID="$2"; shift; shift;;
 		*) PROC_NAME="$1"; shift;;
 	esac
 done
 
-PID=$(ps -ef | grep "$PROC_NAME" | grep -v grep | grep -v `basename "$0"` | tail -1 | awk '{print $2}')
+if [ "$PROC_NAME" != "indigo_worker" ] && [ ! -z "$PID" ]; then
+	echo "Process ID (-p | --pid) and process name can not be used together"
+	exit 1;
+fi
+
+if [ -z "$PID" ]; then
+	#PID=$(ps -ef | grep "$PROC_NAME" | grep -v grep | grep -v `basename "$0"` | tail -1 | awk '{print $2}')
+	PID=$(pidof "$PROC_NAME")
+fi
 
 if [ -z "$PID" ]; then
 	echo "Process '$PROC_NAME' not found."
-	echo "To change process name, use: $0 <process_name>"
+	echo "To change process name or specify PID, use:"
+	echo "    $0 [-v] <process_name>"
+	echo "    $0 [-v] -p <pid>"
 	exit 1;
 fi
+
+PROC_NAME=$(ps -q $PID -o comm=)
 
 echo "Inspecting '$PROC_NAME' (pid = $PID) for deadlocks"
 
